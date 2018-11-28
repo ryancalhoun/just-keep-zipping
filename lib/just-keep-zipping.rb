8a2643e6b7df7d3d02e410496468c9f23368b8b6
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2018-11-28 13:55:50 +0000

    Adds method documentation

--- lib/just-keep-zipping.rb
+++ lib/just-keep-zipping.rb
@@ -1,17 +1,45 @@
 require 'zip'
 
+# Allows the creating of large ZIP files in a streaming fashion.
+#
+# Example:
+#   zip = JustKeepZipping.new
+#   zip.add 'file1.txt', 'Data to be zipped'
+#   data1 = zip.read
+#   progress = Marshal.dump zip # into an object store?
+#
+#   zip = Marshal.load progress # load from object store?
+#   zip.add 'file2.txt', 'More data to be zipped'
+#   zip.close
+#   data2 = zip.read
+#
+#   complete_archive = data1 + data2
+#
 class JustKeepZipping
   attr_reader :entries
 
+  # Use the constructor for the initial object creation.
+  # Use Marshal.dump and Marshal.load (e.g. with Redis) to tranfer this instance between
+  # compute units (e.g. Sidekiq jobs).
+  #
   def initialize
     @entries = []
     @data = ''
   end
 
+  # The current data size. Use this as a stopping or checkpoint condition, to
+  # keep memory from growing too large.
+  #
   def current_size
     @data.size
   end
 
+  # Add a file to the archive.
+  #
+  # Params:
+  # +filename+:: a string representing the name of the file as it should appear in the archive
+  # +body+:: a string or IO object that represents the contents of the file
+  #
   def add(filename, body)
     io = Zip::OutputStream.write_buffer do |zip|
       zip.put_next_entry filename
@@ -31,6 +59,10 @@ class JustKeepZipping
     nil
   end
 
+  # Finalizes the archive by adding the trailing ZIP header. A final read must be called to get the data.
+  #
+  # No further files should be added after calling close.
+  #
   def close
     contents_size = 0
     @entries.each do |e|
@@ -51,6 +83,11 @@ class JustKeepZipping
     contents_size
   end
 
+  # Get the current ZIP data, to save in an object store like S3 or GCS.
+  #
+  # Do this before persisting this instance with Marshal.dump, to avoid
+  # placing too much progress data into a temporary object store like Redis.
+  #
   def read
     data = @data
     @data = ''
